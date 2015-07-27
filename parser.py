02bde2436bd8421cd845c878b005fb57802e2a2b
tinray <b03902072@ntu.edu.tw>
2015-07-27 10:14:33 +0800

    add function parse general exam

--- parser.py
+++ parser.py
@@ -60,7 +60,20 @@ def skip_GSAP_head( tag_list, start_index ):
         parsed_str += str( tag_list[start_index] )
         start_index += 1
     return { 'parsed_str':parsed_str, 'next_index':start_index}
-        
+
+
+def extract_exam_head( tag_list, start_index ):
+    '''Extract the head of general exam.
+    --> go through until encounter subsection title
+    '''
+    parsed_str = "" 
+    while check_GSAP_subsection_title( tag_list, start_index ) == 0:
+        parsed_str += str( tag_list[start_index] )
+        start_index += 1
+    return { 'parsed_str':parsed_str, 'next_index':start_index}
+
+
+
 def get_multi_opt_description( tag_list, start_index, opt_type ):
     '''
     Get the question part of a mulitiple choise problem.
@@ -79,6 +92,7 @@ def get_multi_opt_description( tag_list, start_index, opt_type ):
         tag = tag_list[start_index]    
         
     return { 'parsed_str':parsed_str, 'next_index':start_index}
+
 
 
 def get_multi_opt_answers( tag_list, start_index, opt_type ):
@@ -171,10 +185,13 @@ def parse_multi_opt_question( tag_list, start_index, opt_type ):
 
 def parse_multi_opt_question_part( tag_list, start_index, opt_type ):
     '''Return list of questions of a part.'''
+
+    n_tags = len( tag_list )
+    
     questions = []
     
     # Parse questions until encounter title of any level
-    while check_GSAP_subsection_title( tag_list, start_index ) == 0 :
+    while start_index < n_tag and check_GSAP_subsection_title( tag_list, start_index ) == 0 :
         if check_blank_line( tag_list, start_index ):
             start_index += 1
             continue        
@@ -183,7 +200,35 @@ def parse_multi_opt_question_part( tag_list, start_index, opt_type ):
         questions.append( result['question'] )
 
     return {'questions':questions, 'next_index':start_index }
+
+def parse_true_false_question_part( tag_list, start_index ):
+
+    n_tags = len( tag_list )
+
+    questions = []
+    while start_index < n_tag and check_GSAP_subsection_title( tag_list, start_index ) == 0:
+        questions.append( str( tag_list[start_index] ) )
+        start_index += 1
+        while not re.match( '^[ ]*[0-9]*[ ]*\.' ):
+            questions.append( str( tag_list[start_index] ) )
+            start_index += 1
+    return { 'questions': questions, 'next_index': start_index }
+
+def parse_fill_in_the_blank_question_part( tag_list, start_index ):
+
+    n_tags = len( tag_list )
     
+    questions = []
+    while start_index < n_tags and check_GSAP_subsection_title( tag_list, start_index ) == 0:
+        questions.append( str( tag_list[start_index] ) )
+        start_index += 1
+        while not re.match( '^[ ]*[0-9]*[ ]*\.' ):
+            questions.append( str( tag_list[start_index] ) )
+            start_index += 1
+    return { 'questions': questions, 'next_index': start_index }
+
+
+
 def find_img_belong_to( exam ):
     '''
     For all the images in the exam, 
@@ -200,12 +245,12 @@ def find_img_belong_to( exam ):
 
         #For all questions in the part
         for question in part['questions']:
-            img_in_question = []
+            img_in_question = set()
 
             #Find all the images in the description
             img_tags = BeautifulSoup( question['description'], 'lxml' ).find_all('img')
             for img_tag in img_tags:
-                img_in_question.append( img_tag.attrs['src'] )
+                img_in_question.add( img_tag.attrs['src'] )
 
             #For all the options in the question
             for key, opt in question['answers'].items():
@@ -213,15 +258,34 @@ def find_img_belong_to( exam ):
                 #Find all the images in the option
                 img_tags = BeautifulSoup( opt, 'lxml' ).find_all('img')
                 for img_tag in img_tags:
-                    img_in_question.append( img_tag.attrs['src'] )
+                    img_in_question.add( img_tag.attrs['src'] )
 
-            img_in_part.append(img_in_question )
+            img_in_part.append( list( img_in_question ) )
 
         img_in_exam.append( img_in_part )
         
     return img_in_exam
-    
 
+def analyze_following_question_type( tag_list, start_index ):
+    '''
+    Analyze the type of following questions form the title.
+    ( if the title comtains specific keyword, 
+    then assert that the following questions is of the type )
+    '''
+
+    #Each type has its keywords in the title.    
+    type_keywords = { 'true_false' : [ '是非題', '圈圈叉叉' ],
+                      'fill_in_blank': [ '填充題', '填空題' ],
+                      'multi_option': ['選擇題']
+                      }
+    title_text = tag_list[start_index].text
+    for question_type, keywords in type_keywords:
+        for keyword in keywords:
+            if title_text.find( keyword ) != -1:
+                return question_type 
+
+    return 'unknown'
+    
 def parse_GSAP( tag_list, opt_type ):
     '''Parse the whole contain of GSAP exam'''    
     start_index = 0
@@ -250,7 +314,48 @@ def parse_GSAP( tag_list, opt_type ):
               ]
               }
 
+def parse_general_question( tag_list, opt_type ):
+    '''Parse the whole contain of general exam'''    
+    parsed_exam = {}
 
+    n_tags = len(tag_list)
+    
+    start_index = 0
+    result = extract_exam_head( tag_list, start_index )
+    start_index = result['next_index']
+    parsed_exam['exam_head'] = result['parsed_str']
+
+    #For every part of question in the exam
+    parsed_exam['question_parts'] = []
+    while start_index < n_tags:
+        question_part = {}
+
+        result = extract_GSAP_subsection_title1( tag_list, start_index )
+        start_index = result['next_index']
+        question_part['title'] = result['parsed_str']
+        
+        question_type = analyze_following_question_type( question_part['title'] )
+
+        # Base on the question type, call different function
+        if question_type == 'multi_option':
+            result = parse_multi_opt_question( tag_list, start_index, opt_type )
+        elif question_type == 'true_false':
+            result = parse_true_false_question_part( tag_list, start_index )
+        elif question_type == 'fill_in_blank':
+            result = parse_fill_in_the_blank_question_part( tag_list, start_index )
+        else:
+            print( "Error: in parse_general_exam, unknow question type: ",
+                   question_type, file=sys.stderr )
+            return
+
+        start_index = result['next_index']
+
+        question_part['questions'] = result['questions']
+        parsed_exam['question_parts'].append( question_part )
+                
+    return parsed_exam
+    
+    
 def make_soup( html_bytes, encoding ):
     print( "LOG: Encoding html to Python string...", file=sys.stderr )
     raw_html = html_bytes.decode( encoding, 'ignore' )
@@ -304,17 +409,11 @@ def get_html_encodeing(soup):
 print( "LOG: Opening file...", file=sys.stderr )
 html_bytes = open( sys.argv[1], 'rb' ).read()
 
+#set big5 as default encoding and use it
 default_encoding = "big5"
 encoding = default_encoding
-# print( "LOG: Auto detect html encoding...", file=sys.stderr )
-# encoding = chardet.detect( html_bytes )['encoding']
-# print( "LOG: Detected encoding: ", encoding, file=sys.stderr  )
 
 soup = make_soup( html_bytes, encoding )
-
-#All tags meaningful is under html -> body -> div -> { paragraphs, div, span, blah ~ }
-#Therefore, the tag list contains those {paragraphs, div, span, blah~ }
-tag_list = soup.html.body.div.find_all(True, recursive=False)
 
 print( "LOG: Rechecking encoding according to html meta charset", file=sys.stderr )
 encoding = get_html_encodeing( soup )
@@ -323,14 +422,20 @@ if encoding != default_encoding:
     print( "WARN: The encoding of the file is not default encoding(",
            default_encoding,
            ")" )
-    print( "LOG: Try to decode html file with detected encode" )
+    print( "LOG: Try to decode html file with detected encode: ", encoding )
     soup = make_soup( html_bytes, encoding )
+
+
+#All tags meaningful is under html -> body -> div -> { paragraphs, div, span, blah ~ }
+#Therefore, the tag list contains those {paragraphs, div, span, blah~ }
+tag_list = soup.html.body.div.find_all(True, recursive=False)
 
 
 opt_type = analyze_option_type( soup )
 print( "LOG: Analyzed option type = ", opt_type, file=sys.stderr )
 
-exam = parse_GSAP(tag_list, opt_type)
+#exam = parse_GSAP(tag_list, opt_type)
+exam = parse_general_exam( tag_list, opt_type )
 img_belongings = find_img_belong_to( exam )
 
 output = { 'exam': exam,
